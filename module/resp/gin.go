56296ba1f94ff176996499211b3a6413aa641972
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-07-22 19:14:20 +0800

    fix: use ptr struct resp

--- module/resp/gin.go
+++ module/resp/gin.go
@@ -9,44 +9,44 @@ import (
 
 // OK 返回成功信息
 func OK(c *gin.Context) {
-	resp := map[string]interface{}{
+	resp := &gin.H{
 		"success": true,
 	}
-	c.JSON(http.StatusOK, &resp)
+	c.JSON(http.StatusOK, resp)
 }
 
 // OKWithData 返回成功信息，携带自定义数据（结构体）
 func OKWithData(c *gin.Context, data interface{}) {
-	resp := map[string]interface{}{
+	resp := &gin.H{
 		"success": true,
 		"data":    data,
 	}
 
-	c.JSON(http.StatusOK, &resp)
+	c.JSON(http.StatusOK, resp)
 }
 
 // Abort 返回错误码
 func Abort(c *gin.Context, code code.Code) {
-	errorResp := map[string]interface{}{
+	errorResp := &gin.H{
 		"code":    code,
 		"message": code.String(),
 	}
-	resp := map[string]interface{}{
+	resp := &gin.H{
 		"success": false,
-		"error":   &errorResp,
+		"error":   errorResp,
 	}
-	c.AbortWithStatusJSON(http.StatusOK, &resp)
+	c.AbortWithStatusJSON(http.StatusOK, resp)
 }
 
 // AbortWithMsg 返回错误码，自定义错误信息
 func AbortWithMsg(c *gin.Context, code code.Code, msg string) {
-	errorResp := map[string]interface{}{
+	errorResp := &gin.H{
 		"code":    code,
 		"message": code.String() + ": " + msg,
 	}
-	resp := map[string]interface{}{
+	resp := &gin.H{
 		"success": false,
 		"error":   &errorResp,
 	}
-	c.AbortWithStatusJSON(http.StatusOK, &resp)
+	c.AbortWithStatusJSON(http.StatusOK, resp)
 }
