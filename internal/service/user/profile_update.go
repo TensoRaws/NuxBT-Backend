a1466a3899886c4ae5a36264ff9ecfdc4c748236
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-07-23 22:23:16 +0800

    feat: torrent edit api

--- internal/service/user/profile_update.go
+++ internal/service/user/profile_update.go
@@ -1,7 +1,6 @@
 package user
 
 import (
-	"github.com/TensoRaws/NuxBT-Backend/dal/model"
 	"github.com/TensoRaws/NuxBT-Backend/internal/common/db"
 	"github.com/TensoRaws/NuxBT-Backend/module/code"
 	"github.com/TensoRaws/NuxBT-Backend/module/log"
@@ -14,7 +13,7 @@ type ProfileUpdateRequest struct {
 	Avatar     string `json:"avatar" binding:"required"`
 	Background string `json:"background" binding:"required"`
 	Email      string `json:"email" binding:"required,email"`
-	Private    bool   `json:"private" binding:"required"`
+	Private    *bool  `json:"private" binding:"required"`
 	Signature  string `json:"signature" binding:"required"`
 	Username   string `json:"username" binding:"required"`
 }
@@ -36,15 +35,14 @@ func ProfileUpdate(c *gin.Context) {
 
 	userID, _ := resp.GetUserIDFromGinContext(c)
 
+	// 没传数字，直接序列化嗯造
+	updateInfo, err := util.StructToMap(req)
+	if err != nil {
+		resp.AbortWithMsg(c, code.UnknownError, err.Error())
+		return
+	}
 	// 执行更新
-	err = db.PatchUser(userID, &model.User{
-		Avatar:     req.Avatar,
-		Background: req.Background,
-		Email:      req.Email,
-		Private:    req.Private,
-		Signature:  req.Signature,
-		Username:   req.Username,
-	})
+	err = db.PatchUser(userID, updateInfo)
 
 	if err != nil {
 		resp.AbortWithMsg(c, code.DatabaseErrorRecordPatchFailed, err.Error())
